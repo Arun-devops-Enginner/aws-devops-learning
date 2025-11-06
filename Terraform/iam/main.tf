f4c83db48fc08170f495db5b3ac283de49db73ad
Arun-devops-Enginner <am9765264@gmail.com>
2025-11-07 01:01:48 +0530

    new user creating using  terraform

---
+++ Terraform/iam/main.tf
@@ -0,0 +1,7 @@
+resource "aws_iam_user" "lb" {
+  name = "loadbalancer"
+
+  tags = {
+    tag-key = "newdevloper"
+  }
+}
---
+++ Terraform/iam/profile.tf
@@ -0,0 +1,3 @@
+provider "aws" {
+  profile = "profile that you configure"
+}
